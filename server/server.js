e4d53c6fc125d29f90f255e7b036ccd65a70b71b
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-01-26 12:30:08 -0500

    [+] Sockets: Servidor Emite & Cliente Escucha

--- server/server.js
+++ server/server.js
@@ -20,6 +20,13 @@ app .use( express .static( publicPath ) );
 io .on( 'connection', ( client ) => {
     console .log( 'Usuario conectado al Servidor' );    // Registro en la consola la conexión del Socket al Servidor
 
+     /** Emite un mensaje al Cliente  */
+    client .emit( 'userData', {         // 'userData' Nombre del evento que identifica el mensaje a Emitir
+            user: 'admin',              // Datos Enviados a través del Socket (Generalmente en formato JSON)
+            message: 'Bienvenido a la Aplicación'
+        } 
+    );
+
     /** Escucha evento de desconección del Cliente */
     client .on( 'disconnect', () => {
         console .log( 'Usuario desconectado!' );
