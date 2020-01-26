2f92ebbe626db6f20f407f5f1007cd0ea88cd644
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-01-26 10:54:52 -0500

    [+] Middleware: Habilita directorio publico

--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
 const express = require( 'express' ),
       app = express(),
+      /** Dependencias Nativas */
+      path = require( 'path' ),
+      /** Proyecto */
+      publicPath = path .resolve( __dirname, '../public' ),
       /** Variables de Entorno */
       port = process.env.PORT || 3000;
+
+/** Middlewares */
+app .use( express .static( publicPath ) );
 
 /** Lanza el Servidor */
 app .listen( port, ( error ) => {
