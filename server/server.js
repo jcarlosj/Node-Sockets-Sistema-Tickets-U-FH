5a0737c76a1cc3bf526bec14b465d4e9dd45f950
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-01-26 11:14:04 -0500

    [+] NPM > socket.io: Instalacion & Configuracion (BackEnd)
    Aclaracion:
    Para verificar la correcta instalacion y configuracion de Sockets debe ser posible
    tener accesso a la libreria de 'socket.io.js' que se consumira desde el FrontEnd
    en:
    
    	http://localhost:3000/socket.io/socket.io.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 const express = require( 'express' ),
       app = express(),
+      socketIO = require( 'socket.io' ),
       /** Dependencias Nativas */
+      http = require( 'http' ),
       path = require( 'path' ),
       /** Proyecto */
       publicPath = path .resolve( __dirname, '../public' ),
+      server = http .createServer( app ),       // Configuración de http con Express
+      io = socketIO( server ),                  /* Inicializa el servicio de Sockets con el Servidor. Mantiene Conexión Activa con el BackEnd
+                                                   NOTA: Como la nombre en el BackEnd debo invocarla en el FrontEnd en este caso 'io'
+                                                */
       /** Variables de Entorno */
       port = process.env.PORT || 3000;
 
@@ -11,7 +17,7 @@ const express = require( 'express' ),
 app .use( express .static( publicPath ) );
 
 /** Lanza el Servidor */
-app .listen( port, ( error ) => {
+server .listen( port, ( error ) => {
     if ( error ) throw new Error( error ) ;
     console .log( `Servidor lanzado en http://localhost:${ port }` );
 }); 
