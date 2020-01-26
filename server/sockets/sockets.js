4b0070707526873a39b792899be8740007f84a97
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-01-26 14:37:10 -0500

    [+] Sockets > Boadcast: Cliente Emite & Servidor Emite/Respuesta a otros Clientes

--- server/sockets/sockets.js
+++ server/sockets/sockets.js
@@ -19,23 +19,11 @@ io .on( 'connection', ( client ) => {
     /** Escucha al Cliente & Recoge los datos Emitidos
      *  - Esta comunicación es 1&1 (Pruebe con varias pestañas en su navegador)
      *  - El Cliente se comunica con el Servidor, pero su mensaje no es visto por otros */
-    client .on( 'userData', ( message, callback ) => {     // 'userData' Nombre del evento esperado
-        console .log( message );
+    client .on( 'userData', ( data, callback ) => {     // 'userData' Nombre del evento esperado
+        console .log( data );
         
-        if( message .user ) {
-            callback({      // Ejecuta el Callback del Evento que emite el mensaje en el FrontEnd
-                success: true,
-                message: 'Datos recibidos'
-            });     
-        }
-        else {
-            callback({      // Ejecuta el Callback del Evento que emite el mensaje en el FrontEnd
-                success: false,
-                error: {
-                    message: 'El nombre del usuario es obligatorio'
-                } 
-            });
-        }
+        /** Emitir mensaje de respuesta a todos los clientes */
+        client .broadcast .emit( 'userData', data );
         
     });
 });
