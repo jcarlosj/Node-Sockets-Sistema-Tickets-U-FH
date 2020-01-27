ea17d622acbd9e81fbd96e287d9e8f9a3b2db049
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-01-27 14:15:53 -0500

    [+] Sockets > Client: Obtiene siguiente Ticket

---
+++ public/js/socket.new-ticket.js
@@ -0,0 +1,15 @@
+/** Sockets */
+var socket = io(),      // Como la hemos definido en el BackEnd debemos invocarla en el FrontEnd. En nuestro caso 'io'
+     label = $( '#lblNuevoTicket' );    // Obtiene el elemento que despliega el nuevo Ticket
+
+$( 'button' ) .on( 'click', function() {
+    console .log( 'Haz hecho click en un boton' );
+
+    /** Emite un mensaje al Servidor 
+     * De esta misma manera usando la consola del navegador puedo enviar datos al Servidor
+     * sin olvidar usar el mismo nombre del evento programado, en nuestro caso 'userData' */
+    socket .emit( 'nextTicket', null, function( newTicket ) {    // CallBack	  
+        console .log( 'Servidor', newTicket );
+        label .text( newTicket );	                
+    });
+});
--- server/sockets/sockets.js
+++ server/sockets/sockets.js
@@ -1,4 +1,6 @@
-const { io } = require( '../server' );
+const { io } = require( '../server' ),
+      { TicketControl } = require( '../classes/ticket-control' ),
+      ticket = new TicketControl();
 
 /** Escucho en el evento de conexión del Cliente. */
 io .on( 'connection', ( client ) => {
@@ -8,4 +10,15 @@ io .on( 'connection', ( client ) => {
     client .on( 'disconnect', () => {
         console .log( 'Usuario desconectado del Servidor!' );
     });
+
+    /** Escucha al Cliente & Recoge los datos Emitidos
+     *  - Esta comunicación es 1&1 (Pruebe con varias pestañas en su navegador)
+     *  - El Cliente se comunica con el Servidor, pero su mensaje no es visto por otros */
+    client .on( 'nextTicket', ( data, callback ) => {     // 'userData' Nombre del evento esperado
+        let numberTicket = ticket .next();
+
+        console .log( 'Solicita', numberTicket );	
+        callback( numberTicket );
+    });
+
 });
