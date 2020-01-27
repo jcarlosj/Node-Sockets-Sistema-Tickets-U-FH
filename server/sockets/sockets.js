4ba3d74e48daa50e780ea0a8d8fc43af76a7a50b
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-01-27 13:06:29 -0500

    [+] Sockets > (Cliente & Servidor): Notifica Conexion & Desconexion

--- server/sockets/sockets.js
+++ server/sockets/sockets.js
@@ -1,1 +1,11 @@
 const { io } = require( '../server' );
+
+/** Escucho en el evento de conexión del Cliente. */
+io .on( 'connection', ( client ) => {
+    console .log( 'Usuario conectado al Servidor!' );    // Registro en la consola la conexión del Socket al Servidor
+
+    /** Escucha evento de desconección del Cliente */
+    client .on( 'disconnect', () => {
+        console .log( 'Usuario desconectado del Servidor!' );
+    });
+});
